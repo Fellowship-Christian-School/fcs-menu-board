241b74fa35f42f98703fe9512adf260787694f15
AMANANTMAN <83408471+AMANANTMAN@users.noreply.github.com>
2025-01-17 16:26:24 +0000

    Doesnt work

--- my-app/src/types/menu.ts
+++ my-app/src/types/menu.ts
@@ -1,19 +1,18 @@
 export interface MenuItem {
-    name: string;
-    image: string;
-    calories?: number;
-  }
-  
-  export interface MenuSection {
-    title: string;
-    items: MenuItem[];
-  }
-  
-  export interface Settings {
-    pageTitle: string;
-    dateFormat: string;
-    menuSections: MenuSection[];
-    madeByText: string;
-  }
-  
-  
+  name: string;
+  image: string;
+  calories?: number;
+}
+
+export interface MenuSection {
+  title: string;
+  items: MenuItem[];
+}
+
+export interface Settings {
+  pageTitle: string;
+  dateFormat: string;
+  menuSections: MenuSection[];
+  madeByText: string;
+}
+
